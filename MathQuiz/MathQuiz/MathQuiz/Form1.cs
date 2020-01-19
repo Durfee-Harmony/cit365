6436c894bf87583ce04f22ca9b2932275a5c2f1a
ryanlukecross <cro16052@byui.edu>
2020-01-18 22:42:43 -0700

    working on form

--- MathQuiz/MathQuiz/MathQuiz/Form1.cs
+++ MathQuiz/MathQuiz/MathQuiz/Form1.cs
@@ -16,5 +16,11 @@ namespace MathQuiz
     {
       InitializeComponent();
     }
-  }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      bool a = false;
+
+        }
+    }
 }
